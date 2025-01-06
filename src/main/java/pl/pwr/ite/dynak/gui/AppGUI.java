e5cc4a0e30badbbe6a4874f52a064e64e3491fd5
Phario <grzedyn@gmail.com>
2025-01-06 20:02:14 +0100

    minor changes

--- src/main/java/pl/pwr/ite/dynak/gui/AppGUI.java
+++ src/main/java/pl/pwr/ite/dynak/gui/AppGUI.java
@@ -5,22 +5,26 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.scene.control.*;
+import javafx.scene.paint.*;
 
 public class AppGUI extends Application {
     @Override
     public void start(Stage stage) {
         var gridPane = new GridPane();
-        var scene = new Scene(gridPane, 600, 400);
         var timeSlider = new Slider();
+        var startButton = new Button("Start");
+        gridPane.setStyle("-fx-background-color: rgb(13, 17, 33)");
+        gridPane.setGridLinesVisible(true);
         gridPane.setHgap(10);
         gridPane.setVgap(10);
-        gridPane.add(timeSlider, 1, 1, 2, 1);
-        timeSlider.setMin(100);
+        gridPane.add(timeSlider, 0, 0);
+        gridPane.add(startButton, 3, 0);
         timeSlider.setMax(10000);
         timeSlider.setShowTickLabels(true);
         timeSlider.setShowTickMarks(true);
         timeSlider.setMajorTickUnit(1000);
         timeSlider.setMinorTickCount(100);
+        var scene = new Scene(gridPane, 600, 400);
         stage.setScene(scene);
         stage.show();
     }
